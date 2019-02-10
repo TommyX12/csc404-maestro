c3421b381f260f36cc1c3fac8eefce8c07438b77
FreakingBarbarians <Paarthurnax@live.ca>
2019-02-10 13:25:54 -0500

    rewired player

---
+++ Assets/PlayerAgent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAgent : BasicAgent
+{
+    public AudioSource OnDeathClip;
+
+    public override void OnDeath()
+    {
+        this.initialHitPoint = 100;
+        transform.position = CheckpointManager.instance.GetActiveCheckpoint().transform.position;
+        OnDeathClip.Play();
+    }
+}
--- Assets/Volumes/HazardZone.cs
+++ Assets/Volumes/HazardZone.cs
@@ -8,19 +8,17 @@ public class HazardZone : MonoBehaviour
     public bool Active = true;
     // some things for tick rate and stuff later
 
+    private void DoDamage(BasicAgent other) {
+        BasicAgent.Event.Damage damage;
+        damage.amount = 100;
+        other.ReceiveEvent(damage);
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player") && Active)
         {
-            collision.gameObject.GetComponent<Damageable>().OnHit(HazardDamage, 1);
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Player") && Active)
-        {
-
+            DoDamage(collision.gameObject.GetComponent<BasicAgent>());
         }
     }
 
@@ -28,15 +26,7 @@ public class HazardZone : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player") && Active)
         {
-            other.gameObject.GetComponent<Damageable>().OnHit(HazardDamage, 1);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player") && Active)
-        {
-            other.gameObject.GetComponent<Damageable>().OnHit(HazardDamage, 1);
+            DoDamage(other.gameObject.GetComponent<BasicAgent>());
         }
     }
 
@@ -44,7 +34,7 @@ public class HazardZone : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player") && Active)
         {
-            other.gameObject.GetComponent<Damageable>().OnHit(HazardDamage, 1);
+            DoDamage(other.gameObject.GetComponent<BasicAgent>());
         }
     }
 }
