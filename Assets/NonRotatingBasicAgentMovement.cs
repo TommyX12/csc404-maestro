c8505a1889f831387261daffd8ac4f98e46bae17
FreakingBarbarians <Paarthurnax@live.ca>
2019-02-10 12:56:57 -0500

    changed some fields in basicagentmovement to protected

--- Assets/NonRotatingBasicAgentMovement.cs
+++ Assets/NonRotatingBasicAgentMovement.cs
@@ -1,64 +1,59 @@
-using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
-public class BasicAgentMovement : AgentMovement {
+public class NonRotatingBasicAgentMovement : BasicAgentMovement
+{
 
-    protected Vector3 up = Vector3.up;
-    protected Vector3 forward = Vector3.fwd;
-    protected Vector3 right = Vector3.right;
-
-    protected Quaternion targetRotation;
-
-    protected Vector3 velocity = Vector3.zero;
-    protected Vector3 force = Vector3.zero;
-    
     // self reference
     protected Rigidbody rigidbody;
 
     // exposed parameters
     public float thrust = 0.01f;
     public float friction = 0.875f;
-    
-    public BasicAgentMovement() {
-        
+
+    public NonRotatingBasicAgentMovement()
+    {
+
     }
 
-    public override void ReceiveEvent(Event.DirectionalMove directionalMove) {
-        if (directionalMove.right != 0 || directionalMove.up != 0) {
+    public override void ReceiveEvent(Event.DirectionalMove directionalMove)
+    {
+        if (directionalMove.right != 0 || directionalMove.up != 0)
+        {
             Vector3 direction = Vector3.ClampMagnitude(directionalMove.right * right + directionalMove.up * forward, 1);
             force = direction * thrust;
-            targetRotation = Quaternion.LookRotation(direction);
         }
     }
 
-    protected void Awake() {
+    protected void Awake()
+    {
         rigidbody = GetComponent<Rigidbody>();
         rigidbody.useGravity = false;
         rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
     }
 
-    protected void Update() {
-        
+    protected void Update()
+    {
+
     }
 
-    protected void UpdateMovement() {
+    protected void UpdateMovement()
+    {
         // apply force
         velocity += force;
         force = Vector3.zero;
-        
+
         // friction
         velocity *= friction;
-        
+
         // rotation and finalize
         transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 0.5f);
         rigidbody.position += velocity;
     }
 
-    protected void FixedUpdate() {
+    protected void FixedUpdate()
+    {
         UpdateMovement();
     }
 }
