e7cbf2415f54cc3884f5138530d5a7455d130eee
FreakingBarbarians <Paarthurnax@live.ca>
2019-02-10 12:28:34 -0500

    added enemy model

---
+++ Assets/DissappearingCubeGroup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissappearingCubeGroup : MonoBehaviour
+{
+    public List<DissappearingBlock> Blocks = new List<DissappearingBlock>();
+    private int idx = -1;
+
+    private void Start()
+    {
+        if (Blocks.Count > 0)
+        {
+            idx = 0;
+        }
+        else {
+            Destroy(this);
+        }
+
+        foreach (DissappearingBlock b in Blocks)
+        {
+            b.Dissappear();
+        }
+    }
+
+    public void OnBeat() {
+        foreach (DissappearingBlock b in Blocks) {
+            b.Dissappear();
+        }
+        Blocks[idx].Appear();
+        idx++;
+        idx %= Blocks.Count;
+    }
+
+
+}
---
+++ Assets/MarkerParticleSystemController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class MarkerParticleSystemController : MonoBehaviour
+{
+    private ParticleSystem psys;
+    private ParticleSystemRenderer psr;
+    public GameObject Marker;
+    private void Start()
+    {
+        psys = GetComponent<ParticleSystem>();
+        psr = GetComponent<ParticleSystemRenderer>();
+    }
+
+    private void Update()
+    {
+        float rot = transform.localEulerAngles.y;
+        psr.alignment = ParticleSystemRenderSpace.Local;
+        float delta = (Marker.transform.position - transform.position).magnitude;
+        var pmain = psys.main;
+        pmain.startLifetime = delta / psys.main.startSpeed.constant;
+        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[psys.main.maxParticles];
+        int pcount = psys.GetParticles(particles);
+        for (int i = 0; i < pcount; i++)
+        {
+            float pdelta = (psys.transform.TransformPoint(particles[i].position) - transform.position).magnitude;
+            if (pdelta > delta)
+            {
+                particles[i].remainingLifetime = 0;
+            }
+        }
+        psys.SetParticles(particles);
+    }
+}
