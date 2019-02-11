caefcecd2d12a17f3538cc6882b6c4e221fdca91
FreakingBarbarians <Paarthurnax@live.ca>
2019-02-11 12:21:15 -0500

    added stuff

---
+++ Assets/ShaderPulse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderPulse : MonoBehaviour
+{
+    public Material material;
+    private int pulseID;
+    float pulse = 0;
+
+    private void Start()
+    {
+        pulseID = Shader.PropertyToID("_pulse");
+    }   
+
+    public void Pulse() {
+        pulse = 1;
+    }
+
+    public void Update()
+    {
+        material.SetFloat(pulseID, pulse);
+        if (pulse > 0) {
+            pulse -= Time.deltaTime;
+        }
+    }
+
+
+}
