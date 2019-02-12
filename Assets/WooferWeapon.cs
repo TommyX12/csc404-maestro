3a3e051af95146a11a8147c954b8ce473658e2fa
FreakingBarbarians <Paarthurnax@live.ca>
2019-02-11 22:38:45 -0500

    temp

--- Assets/WooferWeapon.cs
+++ Assets/WooferWeapon.cs
@@ -7,10 +7,12 @@ public class WooferWeapon : BasicWeapon
     public ParticleSystem MuzzleFlash;
     public ParticleSystem Charge;
 
-    public override void Fire()
+    protected override void OnFire()
     {
-        base.Fire();
+        base.OnFire();
+        // MuzzleFlash.Play();
         MuzzleFlash.Emit(50);
+        // Charge.Play();
         Charge.Emit(50);
     }
 }
