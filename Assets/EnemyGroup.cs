9cbf039c38a144d89f11ba979b79d3d6c3fc4b1b
FreakingBarbarians <Paarthurnax@live.ca>
2019-02-11 22:27:20 -0500

    merged

--- Assets/EnemyGroup.cs
+++ Assets/EnemyGroup.cs
@@ -11,11 +11,6 @@ public class EnemyGroup : MonoBehaviour
 
     private void Update()
     {
-<<<<<<< HEAD
-        // Debug.Log(obj.Count);
-
-=======
->>>>>>> 872ce0f3a9fe9d15d454fd8e54ad9a5e7e0507a0
         List<GameObject> obj2 = new List<GameObject>();
         foreach (GameObject o in obj) {
             if (o) {
--- Assets/WooferWeapon.cs
+++ Assets/WooferWeapon.cs
@@ -4,4 +4,13 @@ using UnityEngine;
 
 public class WooferWeapon : BasicWeapon
 {
+    public ParticleSystem MuzzleFlash;
+    public ParticleSystem Charge;
+
+    public override void Fire()
+    {
+        base.Fire();
+        MuzzleFlash.Emit(50);
+        Charge.Emit(50);
+    }
 }
