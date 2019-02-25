7b6662a3feb0ab7fae1c30e6187d427e67bd8305
FreakingBarbarians <Paarthurnax@live.ca>
2019-02-25 17:08:56 -0500

    added particles for the pyramid proj, added sequencer

---
+++ Assets/BeatSequencer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class BeatSequencer : MonoBehaviour
+{
+    public bool useClip;
+    public AudioClip clip;
+    public int bpm = 1;
+    public int beatNum;
+    public int trackNum;
+
+    [HideInInspector]
+    public bool[] tracks = null;
+
+    public Texture2D sequencerOnTextureA;
+    public Texture2D sequencerOffTextureA;
+    public Texture2D sequencerOnTextureB;
+    public Texture2D sequencerOffTextureB;
+}
