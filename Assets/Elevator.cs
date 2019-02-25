0e1dcf3a6fa41ad5bd2b1b131885ae95dfc0c306
Pieman-Manderly <Pieman-Manderly@users.noreply.github.com>
2019-02-24 19:35:41 -0500

    Added a changing walls level.
    It's ShiftingWalls. I admit I should rename it to something like "DisappearingWalls".

---
+++ Assets/Elevator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public GameObject moveplatform;
+    private bool move;
+    private void OnTriggerStay()	{
+    	moveplatform.transform.position += moveplatform.transform.up * Time.deltaTime;
+    	//moveplatform.transform.position += moveplatform.transform.right * Time.deltaTime;
+    }
+}
