b3c22d5cae73d341588be5c10df44f83b913aab6
FreakingBarbarians <Paarthurnax@live.ca>
2019-02-25 22:13:46 -0500

    added postprocessing

---
+++ Assets/DummyEnemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyEnemy : BasicAgent
+{
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+        this.type = Agent.Type.ENEMY;
+        AgentManager.current.AddAgent(this);
+    }
+}
