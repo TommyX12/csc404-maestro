2e25e9740b054f86f13375b9ded49f2c7489903d
FreakingBarbarians <Paarthurnax@live.ca>
2019-02-26 16:33:01 -0500

    added some flow to alpha

---
+++ Assets/AudioFinishTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AudioFinishTrigger : MonoBehaviour
+{
+    public AudioSource source;
+    bool playing = false;
+    public UnityEvent onFinish;
+
+    public void Play() {
+        playing = true;
+        source.Play();
+    }
+
+    private void Update()
+    {
+
+        if (playing) {
+            if (!source.isPlaying) {
+                playing = false;
+                onFinish.Invoke();
+            }
+        }
+
+    }
+
+
+}
