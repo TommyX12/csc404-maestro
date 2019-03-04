03ea29587decffbf93870f4250d0f81e5328af49
Jonny <jonathan.boyes@mail.utoronto.ca>
2019-03-04 15:24:39 -0500

    Added new Fire Trap VFX

---
+++ Assets/ParticleAssets/FireTrap/FireKiller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireKiller : MonoBehaviour
+{
+    private ParticleSystem PS;
+    //private Light loghtLoc;
+    //public Light lightComp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PS = GetComponent<ParticleSystem>();
+        //loghtLoc = Instantiate(lightComp, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PS){
+        	if(!PS.IsAlive())
+        	{
+        		//Destroy(loghtLoc)
+        		Destroy(gameObject);
+        	}
+        }
+    }
+}
