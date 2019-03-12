e50152c447afc63c26b93703bf29986da09258ff
FreakingBarbarians <Paarthurnax@live.ca>
2019-03-12 13:03:17 -0400

    changes

---
+++ Assets/SimpleMaterialPulse.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class SimpleMaterialPulse : MonoBehaviour
+{
+    public float frequency = 1;
+    public float offset = 0;
+
+    public Color color1 = Color.black;
+    public Color color2 = Color.white;
+
+    private MaterialPropertyBlock block;
+    private int colorID;
+
+    private Renderer rendy;
+
+    private void Awake()
+    {
+        block = new MaterialPropertyBlock();
+        colorID = Shader.PropertyToID("_Color");
+        rendy = GetComponent<Renderer>();
+    }
+
+    private void Update()
+    {
+        Color col = Color.Lerp(color1, color2, (Mathf.Sin((Time.fixedTime + offset)*frequency) + 1f)/2f);
+        block.SetColor(colorID, col);
+        rendy.SetPropertyBlock(block);
+    }
+
+    private void OnDisable()
+    {
+        rendy.SetPropertyBlock(null);
+    }
+
+}
---
+++ Assets/SpeakerEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeakerEnemy : BasicAgent
+{
+    public PoolableAudioSource deathNoisePrefab;
+    public ParticleGroup deathExplosion;
+
+    private new void Start()
+    {
+        base.Start();
+        AgentManager.current.AddAgent(this);
+        this.type = Agent.Type.ENEMY;
+        foreach (BasicWeapon weapon in weapons) {
+            weapon.SetAutoFire(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
