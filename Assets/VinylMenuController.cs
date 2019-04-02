f15cf5252be9fbdabef21d2bd2a9a58477a74769
FreakingBarbarians <Paarthurnax@live.ca>
2019-04-02 16:43:39 -0400

    removed raycasting from screen flash

---
+++ Assets/VinylMenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VinylMenuController : MonoBehaviour
+{
+    public VinylMenuTextController textController;
+    // Start is called before the first frame update
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S)) {
+            textController.ScrollDown();
+        }
+        if (Input.GetKeyDown(KeyCode.W)) {
+            textController.ScrollUp();
+        }
+    }
+}
